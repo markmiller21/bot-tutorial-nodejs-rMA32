4d4de8218b0cb58868ec7d0a3d8591cfdac2e716
Mark Miller <mark.kilpatrick.miller@gmail.com>
2019-08-22 21:31:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     //postMessage("http://www.daddyleagues.com/maddenrating?name=&position=all&team="+request.text.substring(5,8));
-    postMessage("http://daddyleagues.com/nml18/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/rg/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   
@@ -103,14 +103,14 @@ function respond() {
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
     
-    postMessage("http://daddyleagues.com/nml18/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/rg/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/nml18/players?name="+rep+"&position=all&team=all");
+    postMessage("http://daddyleagues.com/rg/players?name="+rep+"&position=all&team=all");
     
     this.res.end();
   }  
